d7714ae54eb542f6b3215776b53764088574ff8c
nomadics9 <binaskar9@gmail.com>
2024-10-12 19:13:00 +0300

    Shell aliases

--- home/features/cli/zsh.nix
+++ home/features/cli/zsh.nix
@@ -23,15 +23,13 @@ in
       # '';
 
       shellAliases = {
-        switchhypr = "sudo nixos-rebuild switch --flake .#hyprland";
-        switchuhypr = "sudo nixos-rebuild switch --upgrade --flake .#hyprland";
-        switchgnome = "sudo nixos-rebuild switch --flake .#gnome";
-        switchugnome = "sudo nixos-rebuild switch --upgrade --flake .#gnome";
+        switchhypr = "sudo nixos-rebuild switch --flake .";
+        dotfilesu = "nix flake lock --update-input dotfiles";
+        switchuhypr = "nix flake lock --update-input dotfiles && sudo nixos-rebuild switch --flake .";
         clean = "sudo nix-collect-garbage -d";
         cleanold = "sudo nix-collect-garbage --delete-old";
         cleanboot = "sudo /run/current-system/bin/switch-to-configuration boot";
         nvim = "kitty @ set-spacing padding=0 && /run/current-system/sw/bin/nvim";
-        zed = "steam-run zed";
       };
       initExtraFirst = "
     unsetopt beep
